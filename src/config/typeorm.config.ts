b49fbc7dc095a3fd707df93510223ed578eccddd
Ayari17 <ayari17@hotmail.fr>
2020-09-22 18:02:25 +0100

    fixing typeORM config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,21 +1,37 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
 import * as PostgressConnectionStringParser from 'pg-connection-string';
 
+let typeOrmConfig: TypeOrmModuleOptions;
+let connectionOptions = null;
+if (process.env.DATABASE_URL) {
+  connectionOptions = PostgressConnectionStringParser.parse(
+    process.env.DATABASE_URL,
+  );
+}
 
-
-const connectionOptions = PostgressConnectionStringParser.parse(process.env.DATABASE_URL);
-
-
-const dbConfig = config.get('db'); 
-
-export const typeOrmConfig: TypeOrmModuleOptions = {
-         type: dbConfig.type,
-         host: connectionOptions.host || dbConfig.host,
-         port: connectionOptions.port || dbConfig.port,
-         username: connectionOptions.user || dbConfig.username,
-         password: connectionOptions.password || dbConfig.password,
-         database: connectionOptions.database || dbConfig.database,
-         entities: [__dirname + '/../**/*.entity.{ts,js}'],
-         synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
-       };
+const dbConfig = config.get('db');
+if (connectionOptions) {
+  typeOrmConfig = {
+    type: dbConfig.type,
+    host: connectionOptions.host || dbConfig.host,
+    port: connectionOptions.port || dbConfig.port,
+    username: connectionOptions.user || dbConfig.username,
+    password: connectionOptions.password || dbConfig.password,
+    database: connectionOptions.database || dbConfig.database,
+    entities: [__dirname + '/../**/*.entity.{ts,js}'],
+    synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  };
+} else {
+   typeOrmConfig = {
+    type: dbConfig.type,
+    host:  dbConfig.host,
+    port:  dbConfig.port,
+    username: dbConfig.username,
+    password:  dbConfig.password,
+    database:  dbConfig.database,
+    entities: [__dirname + '/../**/*.entity.{ts,js}'],
+    synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  };
+}
+export {typeOrmConfig};
